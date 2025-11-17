61627712cfccbc0b44e94ba9b23c67659647f4d8
GGgordelis <257642025@eniac.edu.br>
2025-11-17 12:26:45 -0300

    Update exercicios.js

--- exercicios.js
+++
@@ -1,8 +0,0 @@
-<script>
-    function toggleProfileMenu() {
-        const menu = document.getElementbyId("profileMenu");
-        menu.style.display = menu.style.display === "block" ? "none" : "block";
-    }
-
-    document.querySelector(".profile-icon").addEventListener("click" , toggleProfileMenu);
-</script>
