be2dd19a35df86f5426e5b668763d0e8e1c135a5
Arthur Pessoa da Silva <arthurpessoadasilva192@gmail.com>
2025-11-17 11:04:48 -0300

    Update Apresentacao.js

--- Apresentacao.js
+++ Apresentacao.js
@@ -2,12 +2,10 @@ function irparacadastro(){
     window.location.href = "cadastro.html";
  }
 
-<<<<<<< HEAD
  function irparalogin(){
     window.location.href = "login.html";
  }
-=======
+
 function irparalogin(){
     window.location.href = "login.html";
  }
->>>>>>> 295526c5fdeea1fec4b391b09ba381718683bf10
